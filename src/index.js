c12e4aadbf139134d1ce7ed2e9fca25ea73fec69
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-26 14:57:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
 /* eslint-disable valid-typeof */
+
+/**
+ * @namespace IRS
+ */
+/**
+* @ignore
+*/
 const type = y => x => typeof x === y;
-
+/**
+* @ignore
+*/
 const isObject = type('object');
+/**
+* @ignore
+*/
 const isFunction = type('function');
-
+/**
+ * Test whether an object implements the [Publisher](https://github.com/reactive-streams/reactive-streams-js#1-publisher-code) interface.
+ * @param {object} publisher
+ * An object that is expected to implement the [Publisher](https://github.com/reactive-streams/reactive-streams-js#1-publisher-code) interface.
+ * @returns {boolean}
+ */
 export const isPublisher = publisher => isObject(publisher) && isFunction(publisher.subscribe);
 
+/**
+ * Test whether an object implements the [Subscriber](https://github.com/reactive-streams/reactive-streams-js#2-subscriber-code) interface.
+ * @param {object} subscriber
+ * An object that is expected to implement the [Subscriber](https://github.com/reactive-streams/reactive-streams-js#2-subscriber-code) interface.
+ * @returns {boolean}
+ */
 export const isSubscriber = subscriber => isObject(subscriber)
   && isFunction(subscriber.onSubscribe)
   && isFunction(subscriber.onNext)
   && isFunction(subscriber.onError)
   && isFunction(subscriber.onComplete);
 
+/**
+ * Test whether an object implements the [Subscription](https://github.com/reactive-streams/reactive-streams-js#3-subscription-code) interface.
+ * @param {object} subscription
+ * An object that is expected to implement the [Subscription](https://github.com/reactive-streams/reactive-streams-js#3-subscription-code) interface.
+ * @returns {boolean}
+ */
 export const isSubscription = subscription => isObject(subscription)
   && isFunction(subscription.request)
   && isFunction(subscription.cancel);
 
+/**
+ * Test whether an object implements the [Processor](https://github.com/reactive-streams/reactive-streams-js#4processor-code) interface.
+ * @param {object} processor
+ * An ojbect that is expected to implement the [Processor](https://github.com/reactive-streams/reactive-streams-js#4processor-code) interface.
+ * @returns {boolean}
+ */
 export const isProcessor = processor => isPublisher(processor) && isSubscriber(processor);
