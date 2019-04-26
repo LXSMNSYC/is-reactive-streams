bd8b2ea67a52ade4c0e3b53d81d40dc5100de786
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-26 14:37:13 +0800

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+var IRS = (function (exports) {
+  'use strict';
+
+  /* eslint-disable valid-typeof */
+  const type = y => x => typeof x === y;
+
+  const isObject = type('object');
+  const isFunction = type('function');
+
+  const isPublisher = publisher => isObject(publisher) && isFunction(publisher.subscribe);
+
+  const isSubscriber = subscriber => isObject(subscriber)
+    && isFunction(subscriber.onSubscribe)
+    && isFunction(subscriber.onNext)
+    && isFunction(subscriber.onError)
+    && isFunction(subscriber.onComplete);
+
+  const isSubscription = subscription => isObject(subscription)
+    && isFunction(subscription.request)
+    && isFunction(subscription.cancel);
+
+  const isProcessor = processor => isPublisher(processor) && isSubscriber(processor);
+
+  exports.isProcessor = isProcessor;
+  exports.isPublisher = isPublisher;
+  exports.isSubscriber = isSubscriber;
+  exports.isSubscription = isSubscription;
+
+  return exports;
+
+}({}));
